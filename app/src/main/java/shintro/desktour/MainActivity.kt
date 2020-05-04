e67653b67e4e1455649c32b891d5ce9ffa23ad23
kumedashintarou <pii.k2.bys@gmail.com>
2020-05-04 23:05:09 +0900

    bottom navigation fragments 追加

--- app/src/main/java/shintro/desktour/MainActivity.kt
+++ app/src/main/java/shintro/desktour/MainActivity.kt
@@ -3,29 +3,114 @@ package shintro.desktour
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+
 import android.widget.Toast
+import androidx.viewpager.widget.ViewPager
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var youtubeBtn: ImageButton
+    private lateinit var homeBtn: ImageButton
+    private lateinit var parsonBtn: ImageButton
+    private lateinit var addBtn: ImageButton
+
+    private lateinit var mViewPager: ViewPager
+    private lateinit var mPagerViewAdapter: PagerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        login_button_main.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+        // init views
+        mViewPager = findViewById(R.id.mViewPager)
+
+        // init image buttons
+        youtubeBtn = findViewById(R.id.youtubeBtn)
+        homeBtn = findViewById(R.id.homeBtn)
+        addBtn = findViewById(R.id.addBtn)
+        parsonBtn = findViewById(R.id.parsonBtn)
+
+        //onclick listner
+
+
+        youtubeBtn.setOnClickListener {
+            mViewPager.currentItem = 0
         }
 
-        register_button_main.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
+        homeBtn.setOnClickListener {
+            mViewPager.currentItem = 1
         }
 
-        signout_button_main.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            Toast.makeText(this, "サインアウトしました。 ", Toast.LENGTH_LONG).show()
+        addBtn.setOnClickListener {
+            mViewPager.currentItem = 2
+        }
+
+        parsonBtn.setOnClickListener {
+            mViewPager.currentItem = 3
+        }
+
+
+
+
+
+
+
+        mPagerViewAdapter = PagerViewAdapter(supportFragmentManager)
+        mViewPager.adapter = mPagerViewAdapter
+        mViewPager.offscreenPageLimit = 1
+
+        // add page change listener
+        mViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                changeTabs(position)
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {}
+        })
+
+        mViewPager.currentItem = 1
+        homeBtn.setImageResource(R.drawable.ic_home_palevioletred)
+
+    }
+
+    private fun changeTabs(position: Int) {
+        if (position == 0) {
+            youtubeBtn.setImageResource(R.drawable.ic_slideshow_palevioletred)
+            homeBtn.setImageResource(R.drawable.ic_home_black)
+            parsonBtn.setImageResource(R.drawable.ic_add_box_black)
+            youtubeBtn.setImageResource(R.drawable.ic_person_outline_black)
+
+        }
+        if (position == 1) {
+            youtubeBtn.setImageResource(R.drawable.ic_slideshow_black)
+            homeBtn.setImageResource(R.drawable.ic_home_palevioletred)
+            parsonBtn.setImageResource(R.drawable.ic_add_box_black)
+            youtubeBtn.setImageResource(R.drawable.ic_person_outline_black)
+
+        }
+        if (position == 2) {
+            youtubeBtn.setImageResource(R.drawable.ic_slideshow_black)
+            homeBtn.setImageResource(R.drawable.ic_home_black)
+            parsonBtn.setImageResource(R.drawable.ic_add_box_palevioletred)
+            youtubeBtn.setImageResource(R.drawable.ic_person_outline_black)
+
+        }
+        if (position == 3) {
+            youtubeBtn.setImageResource(R.drawable.ic_slideshow_black)
+            homeBtn.setImageResource(R.drawable.ic_home_black)
+            parsonBtn.setImageResource(R.drawable.ic_add_box_black)
+            youtubeBtn.setImageResource(R.drawable.ic_person_outline_palevioletred)
         }
     }
 }
+
---
+++ app/src/main/java/shintro/desktour/PagerViewAdapter.kt
@@ -0,0 +1,35 @@
+package shintro.desktour
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import shintro.desktour.Fragments.Add
+import shintro.desktour.Fragments.Home
+import shintro.desktour.Fragments.Parson
+import shintro.desktour.Fragments.YouTube
+
+internal class PagerViewAdapter(fm: FragmentManager?): FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment?{
+        return when(position){
+            0 -> {
+                YouTube()
+            }
+            1 -> {
+                Home()
+            }
+            2 -> {
+                Add()
+            }
+            3 -> {
+                Parson()
+            }
+
+            else -> null
+        }
+    }
+
+    override fun getCount(): Int {
+
+        return 4
+    }
+}
