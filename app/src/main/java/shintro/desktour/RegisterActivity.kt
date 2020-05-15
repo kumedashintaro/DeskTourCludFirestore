3de572fa43173e6999128069c092be273be51743
kumedashintarou <pii.k2.bys@gmail.com>
2020-05-15 22:12:27 +0900

    RegisterActivity 不要なコード削除

--- app/src/main/java/shintro/desktour/RegisterActivity.kt
+++ app/src/main/java/shintro/desktour/RegisterActivity.kt
@@ -35,17 +35,16 @@ class RegisterActivity : AppCompatActivity() {
         login_button_register.setOnClickListener {
             performRegister()
         }
-
     }
 
-    var selectedPhotUri : Uri? = null
+    var selectedPhotUri: Uri? = null
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
             // procead and check what the selected image was...
-            Log.d("RegisterActivity","Photo was selected")
+            Log.d("RegisterActivity", "Photo was selected")
 
             selectedPhotUri = data.data
 
@@ -58,7 +57,7 @@ class RegisterActivity : AppCompatActivity() {
     }
 
 
-    private fun performRegister(){
+    private fun performRegister() {
         val email = email_edittext.text.toString()
         val password = paswward_edittext.text.toString()
 
@@ -70,51 +69,54 @@ class RegisterActivity : AppCompatActivity() {
         Log.d("RegisterActivity", "Password: $password")
 
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener{
-                if (!it.isSuccessful)return@addOnCompleteListener
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
 
-                Log.d("RegisterActivity", "Successfully created user with uid:${it.result?.user?.uid}")
+                Log.d(
+                    "RegisterActivity",
+                    "Successfully created user with uid:${it.result?.user?.uid}"
+                )
 
                 uploadImageToFirebaseStorage()
             }
-            .addOnFailureListener{
+            .addOnFailureListener {
                 Toast.makeText(this, "登録に失敗しました、もう一度入力して下さい ", Toast.LENGTH_LONG).show()
                 Log.d("RegisterActivity", "Failed to create user: ${it.message}")
             }
 
     }
 
-    private fun uploadImageToFirebaseStorage(){
-        if(selectedPhotUri == null) return
+    private fun uploadImageToFirebaseStorage() {
+        if (selectedPhotUri == null) return
 
         val filename = UUID.randomUUID().toString()
         val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
 
         ref.putFile(selectedPhotUri!!)
-            .addOnSuccessListener{
-                Log.d("RegisterActivity","Successfully uploaded image: ${it.metadata?.path}")
+            .addOnSuccessListener {
+                Log.d("RegisterActivity", "Successfully uploaded image: ${it.metadata?.path}")
 
-                ref.downloadUrl.addOnSuccessListener{
+                ref.downloadUrl.addOnSuccessListener {
                     Log.d("RegisterActivity", "File Location: $it")
 
                     saveUserToFirebaseDatabase(it.toString())
 
                 }
             }
-            .addOnFailureListener{
+            .addOnFailureListener {
                 //do some logging here
             }
     }
 
-    private fun saveUserToFirebaseDatabase(profileImageUrl: String){
+    private fun saveUserToFirebaseDatabase(profileImageUrl: String) {
         val uid = FirebaseAuth.getInstance().uid ?: ""
         val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
 
         val user = User(uid, username_edittext.text.toString(), profileImageUrl)
 
         ref.setValue(user)
-            .addOnSuccessListener{
-                Log.d("RegisterActivity","Finally we saved the user to Firebase Database")
+            .addOnSuccessListener {
+                Log.d("RegisterActivity", "Finally we saved the user to Firebase Database")
 
                 val intent = Intent(this, MainActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -173,17 +175,13 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
-    private fun myStorageEnable(){
+    private fun myStorageEnable() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         startActivityForResult(intent, 0)
     }
 
 }
-
-//class User(val uid: String, val username: String, val profileImageUrl: String){
-//   constructor() : this("","","")
-//}
 
 
 
