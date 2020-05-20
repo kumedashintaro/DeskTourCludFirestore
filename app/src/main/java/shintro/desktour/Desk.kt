6be4fa4994554e6589e09b2bf1564c1935414285
shintaro kumeda <pii.k2.bys@gmail.com>
2020-05-20 22:36:11 +0900

    constructorを削除

--- app/src/main/java/shintro/desktour/Desk.kt
+++ app/src/main/java/shintro/desktour/Desk.kt
@@ -5,7 +5,4 @@ import kotlinx.android.parcel.Parcelize
 
 
 @Parcelize
-data class Desk(val uid: String, val titel: String, val comment: String, val profileImageUrl: String , val deskuid: String): Parcelable {
-    constructor() : this("", "","", "","")
-
-}
+data class Desk(val uid: String="", val titel: String ="", val comment: String ="", val profileImageUrl: String ="", val deskuid: String =""): Parcelable
--- app/src/main/java/shintro/desktour/User.kt
+++ app/src/main/java/shintro/desktour/User.kt
@@ -4,6 +4,4 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class User(val uid: String, val username: String, val profileImageUrl: String): Parcelable {
-    constructor() : this("","","")
-}
+data class User(val uid: String ="", val username: String="", val profileImageUrl: String=""): Parcelable
