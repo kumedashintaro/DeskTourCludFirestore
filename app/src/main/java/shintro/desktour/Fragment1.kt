e71448f073b3aa53329ef508c3761f080cb57ae9
shintaro kumeda <pii.k2.bys@gmail.com>
2020-05-24 14:24:19 +0900

    BottomNavigation追加 Activity遷移ボタンの削除

---
+++ app/src/main/java/shintro/desktour/Fragment1.kt
@@ -0,0 +1,14 @@
+package shintro.desktour
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Fragment1 : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return  inflater.inflate(R.layout.home_fragment, container, false)
+    }
+}
